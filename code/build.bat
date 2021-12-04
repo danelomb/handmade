8d41c80c427721140052ed711a873e40df54a132
dane <danelomb@gmail.com>
2021-12-03 19:47:53 -0800

    test2

--- code/build.bat
+++ code/build.bat
@@ -3,5 +3,6 @@
 
 mkdir ..\..\build
 pushd ..\..\build
-cl -Zi w:\code\win32_handmade.cpp
+cl -Zi w:\code\win32_handmade.cpp user32.lib
 popd
+
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -2,11 +2,14 @@
 
 int CALLBACK
 WinMain(HINSTANCE hInstance,
-	HINSTANCE hPrevInstance,
-	LPSTR     lpCmdLine,
-	int       nShowCmd
-)
+		HINSTANCE hPrevInstance,
+		LPSTR     lpCmdLine,
+		int       nShowCmd
+	   )
 
 {
-	return(0);
+		MessageBox(0, "This is Handmade Hero", "Handmade Hero",
+					MB_OK|MB_ICONINFORMATION);
+		return(0);
 }
+
